04c1929e27a3ec7221ced155bf5301cbeb764a57
duckymomo360 <duckymomo360@gmail.com>
2024-11-13 15:37:41 -0800

    Camera cleanup

--- src/modules/Client/Camera/CameraService.lua
+++ src/modules/Client/Camera/CameraService.lua
@@ -4,6 +4,7 @@
 
 local require = require(script.Parent.loader).load(script)
 
+local CameraStackService = require("CameraStackService")
 local TrackCamera = require("TrackCamera")
 
 local CameraService = {}
@@ -13,15 +14,15 @@ function CameraService:Init(serviceBag)
 	assert(not self._serviceBag, "Already initialized")
 	self._serviceBag = assert(serviceBag, "No serviceBag")
 
-	self._cameraStackService = self._serviceBag:GetService(require("CameraStackService"))
+	self._cameraStackService = self._serviceBag:GetService(CameraStackService)
 	self._cameraStackService:SetDoNotUseDefaultCamera(true)
 end
 
 function CameraService:Start()
-	self._camera = TrackCamera.new(workspace.Positions.Camera)
-	self._camera.FieldOfView = 50
+	self._stageCamera = TrackCamera.new(workspace.Positions.Camera)
+	self._stageCamera.FieldOfView = 50
 
-	self._cameraStackService:Add(self._camera)
+	self._cameraStackService:Add(self._stageCamera)
 end
 
 return CameraService
